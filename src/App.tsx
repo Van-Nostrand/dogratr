c03580901e5a305d7c29b2839f0873f87de98510
Daniel Doull <dmdoull43@gmail.com>
2022-09-19 23:22:29 -0700

    added hoc for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react'
 import {
   Routes,
-  Route,
-  Navigate
+  Route
 } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { seedDB } from '@/store/pupper/pupperSlice'
@@ -10,6 +9,7 @@ import Account from '@/pages/Account'
 import Ratr from '@/pages/Ratr'
 import LandingPage from '@/pages/LandingPage'
 import ThreeDTransformTest from '@/components/card-stack/ThreeDTransformTest'
+import ProtectedRoute from '@/hocs/ProtectedRoute'
 import useWindowScrolling from '@/hooks/useWindowScrolling'
 import Login from '@/pages/Login'
 import TopNav from '@/components/TopNav'
@@ -18,7 +18,7 @@ import useToken from '@/hooks/useToken'
 
 export default function App () {
   const dispatch = useDispatch()
-  const { token, setToken } = useToken()
+  const { setToken } = useToken()
 
   useWindowScrolling()
   useEffect(() => {
@@ -33,7 +33,14 @@ export default function App () {
           <Route index element={<LandingPage />} />
           <Route path='/ratr' element={<Ratr />} />
           <Route path='/test' element={<ThreeDTransformTest />} />
-          <Route path='/account' element={ token ? <Account /> : <Navigate replace to='/login' />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute isAuthenticated={false}>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
           <Route path='/login' element={<Login setToken={setToken} />} />
         </Routes>
       </div>
